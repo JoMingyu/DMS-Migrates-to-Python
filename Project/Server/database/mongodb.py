00e36d20c7ae0a8916245f363575144adae32a67
PlanB <city7310@naver.com>
2017-09-19 00:54:45 +0900

    Redesign structure of database
    파이썬에게 쓸모 없는 불필요한 보일러플레이트 제거를 위함

--- Project/Server/database/mongodb.py
+++ Project/Server/database/mongodb.py
@@ -1,3 +1,23 @@
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
+db = client.dms
+
+admin_acc = db.admin_acc
+student_acc = db.student_acc
+
+afterschool_item = db.afterschool_item
+afterschool = db.afterschool
+extension = db.extension
+goingout = db.goingout
+stay = db.stay
+
+bug_report = db.bug_report
+recruit = db.recruit
+
+faq = db.faq
+notice = db.notice
+report_facility = db.report_facility
+rule = db.rule
+
+meal = db.meal
