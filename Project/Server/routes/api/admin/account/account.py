ac5e9f55536d9c978012dffcb8bda6a0945e5719
PlanB <city7310@naver.com>
2017-09-20 18:20:51 +0900

    Seme comment on admin/account/account.py

--- Project/Server/routes/api/admin/account/account.py
+++ Project/Server/routes/api/admin/account/account.py
@@ -1,4 +1,4 @@
-from flask import request, session, Response
+from flask import request, session, Response, abort
 from flask_restful import Resource
 import uuid
 
@@ -9,14 +9,19 @@ from database.mongodb import admin_acc
 
 
 class AddAccount(Resource):
+    """
+    새로운 관리자 계정 추가
+    """
     def post(self):
         if not get_admin_id_from_request(request, session):
-            return '', 403
+            abort(403)
 
         _id = aes.encrypt(request.form.get('id'))
         pw = sha.encrypt(request.form.get('pw'))
         owner = aes.encrypt(request.form.get('owner', '주인 없음', str))
+
         if admin_acc.find_one({'id': _id}):
+            # 이미 가입되어 있는 ID
             return '', 204
         else:
             admin_acc.insert({
@@ -30,18 +35,24 @@ class AddAccount(Resource):
 
 
 class SignIn(Resource):
+    """
+    관리자 계정 로그인
+    """
     def post(self):
         _id = aes.encrypt(request.form.get('id'))
         pw = sha.encrypt(request.form.get('pw'))
         keep_login = request.form.get('keep_login', False, bool)
 
         if admin_acc.find_one({'id': _id, 'pw': pw}):
+            # 로그인 성공
             resp = Response('', 201)
             sid = str(uuid.uuid4())
 
             if keep_login:
+                # 로그인 유지 - 쿠키
                 resp.set_cookie('AdminSession', sid)
             else:
+                # 로그인 비유지 - 세션
                 session['AdminSession'] = sid
 
             data = dict(admin_acc.find_one({'id': _id}))
@@ -49,6 +60,7 @@ class SignIn(Resource):
                 'sid': sid
             })
             admin_acc.update({'id': _id}, data)
+            # SID 업데이트
 
             return resp
         else:
@@ -56,6 +68,9 @@ class SignIn(Resource):
 
 
 class Logout(Resource):
+    """
+    로그아웃
+    """
     def post(self):
         if get_admin_id_from_request(request, session):
             resp = Response('', 201)
