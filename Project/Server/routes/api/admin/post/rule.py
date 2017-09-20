43abd927fec905a73894d27e6e60a755e6b549b7
PlanB <city7310@naver.com>
2017-09-20 16:41:05 +0900

    Implement rule

---
+++ Project/Server/routes/api/admin/post/rule.py
@@ -0,0 +1,58 @@
+from flask import request, session
+from flask_restful import Resource
+
+from support.user.user_manager import get_admin_id_from_request
+
+from database.mongodb import rule
+from pymongo.collection import ObjectId
+
+
+class Rule(Resource):
+    def post(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        rule.insert({
+            'title': title,
+            'content': content
+        })
+
+        return '', 201
+
+    def delete(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        _id = request.form.get('_id')
+
+        rule.remove({
+            '_id': ObjectId(_id)
+        })
+
+        return '', 200
+
+    def get(self):
+        # list
+        data = list(rule.find())
+        for idx in range(len(data)):
+            data[idx]['_id'] = str(data[idx]['_id'])
+
+        return data, 200
+
+    def patch(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        _id = request.form.get('_id')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        rule.update({'_id': ObjectId(_id)}, {
+            'title': title,
+            'content': content
+        })
+
+        return '', 200
