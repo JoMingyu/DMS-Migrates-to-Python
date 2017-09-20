88364c5a8d736619e07c4f518b70fb7a81e93af4
PlanB <city7310@naver.com>
2017-09-20 16:41:00 +0900

    Implement notice

---
+++ Project/Server/routes/api/admin/post/notice.py
@@ -0,0 +1,62 @@
+from flask import request, session
+from flask_restful import Resource
+
+from support.user.user_manager import get_admin_id_from_request
+
+from database.mongodb import notice
+from pymongo.collection import ObjectId
+
+
+class Notice(Resource):
+    def post(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        title = request.form.get('title')
+        content = request.form.get('content')
+        writer = request.form.get('writer', '사감부')
+
+        notice.insert({
+            'title': title,
+            'content': content,
+            'writer': writer
+        })
+
+        return '', 201
+
+    def delete(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        _id = request.form.get('_id')
+
+        notice.remove({
+            '_id': ObjectId(_id)
+        })
+
+        return '', 200
+
+    def get(self):
+        # list
+        data = list(notice.find())
+        for idx in range(len(data)):
+            data[idx]['_id'] = str(data[idx]['_id'])
+
+        return data, 200
+
+    def patch(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        _id = request.form.get('_id')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        writer = request.form.get('writer', '사감부')
+
+        notice.update({'_id': ObjectId(_id)}, {
+            'title': title,
+            'content': content,
+            'writer': writer
+        })
+
+        return '', 200
