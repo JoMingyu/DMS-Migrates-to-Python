a923a363bfb18ce27f2352314af02bcef5ccc018
PlanB <city7310@naver.com>
2017-09-21 10:41:29 +0900

    [API DONE] Admin student account initialize API

---
+++ Project/Server/routes/api/admin/account/initializer.py
@@ -0,0 +1,28 @@
+from flask import request, session
+from flask_restful import Resource
+
+from support.user.user_manager import get_admin_id_from_request
+from support.crypto import *
+
+from database.mongodb import student_acc
+
+
+class InitializeStudent(Resource):
+    """
+    학생 계정을 재가입 가능하도록 초기화
+    """
+    def post(self):
+        if not get_admin_id_from_request(request, session):
+            return '', 403
+
+        number = aes.encrypt(request.form.get('number'))
+
+        data = dict(student_acc.find_one({'number': number}))
+        data.update({
+            'id': None,
+            'pw': None,
+            'sid': None
+        })
+        student_acc.update({'number': number}, data)
+
+        return '', 201
