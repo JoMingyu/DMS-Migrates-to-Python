a1e921dc479ca01a9d4eee146833eab12338fd55
PlanB <city7310@naver.com>
2017-09-22 11:42:39 +0900

    [API DONE] Afterschool Item API

--- Project/Server/routes/api/admin/apply/afterschool.py
+++ Project/Server/routes/api/admin/apply/afterschool.py
@@ -1,4 +1,4 @@
-from flask import request, Session, abort
+from flask import request, session, abort
 from flask_restful import Resource
 
 from support.user.user_manager import get_admin_id_from_request
@@ -7,4 +7,33 @@ from database.mongodb import afterschool_item
 
 
 class AfterschoolItem(Resource):
-    pass
+    """
+    방과후 아이템(POST, GET available)
+    """
+    def post(self):
+        if not get_admin_id_from_request(request, session):
+            abort(403)
+
+        afterschool_item.remove()
+
+        afterschool_data = request.json
+        for afterschool in afterschool_data:
+            title, on_monday, on_tuesday, on_saturday = afterschool.values()
+            afterschool_item.insert({
+                'title': title,
+                'on_monday': on_monday,
+                'on_tuesday': on_tuesday,
+                'on_saturday': on_saturday
+            })
+
+        return '', 201
+
+    def get(self):
+        if not get_admin_id_from_request(request, session):
+            abort(403)
+
+        data = list(afterschool_item.find())
+        for idx, d in enumerate(data):
+            data[idx]['_id'] = str(d['_id'])
+
+        return data
